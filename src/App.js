a0c164c0974df1d3e61dc46e3e848982f27141b4
unknown <arne.n.bjerkaas@gmail.com>
2021-07-25 09:45:22 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [fetchedData, setFetchedData] = useState([]);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [listUpdated, setListUpdated] = useState(false);
+
+  useEffect(() => {
+    const getData = async () => {
+      const data = await axios.get("http://localhost:3001/customerlist");
+      setFetchedData(data);
+    };
+    getData();
+  }, [listUpdated]);
+
+  console.log("data: ", fetchedData);
+
+  const handleFirstNameChange = (e) => {
+    setFirstName(e.target.value);
+  };
+
+  const handleLastNameChange = (e) => {
+    setLastName(e.target.value);
+  };
+
+  const addCustomerHandler = async () => {
+    setListUpdated(true);
+    const newCustomer = {
+      customerFirstName: firstName,
+      customerLastName: lastName,
+    };
+    await axios.post("http://localhost:3000/customer", newCustomer);
+    setFirstName("");
+    setLastName("");
+    setListUpdated(false);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Customers</h1>
+      {fetchedData.data ? (
+        <ul>
+          {fetchedData.data.map((customer, index) => {
+            return (
+              <li
+                key={index}
+              >{`${customer.customerFirstName} ${customer.customerLastName}`}</li>
+            );
+          })}
+        </ul>
+      ) : null}
+      <div>
+        <div>
+          <div>First Name</div>
+          <input
+            type="text"
+            value={firstName}
+            onChange={handleFirstNameChange}
+          />
+        </div>
+        <div>
+          <div>Last Name</div>
+          <input type="text" value={lastName} onChange={handleLastNameChange} />
+        </div>
+        <button onClick={addCustomerHandler}>Add new customer</button>
+      </div>
     </div>
   );
 }
